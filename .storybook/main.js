bc3e60b59594233902109df8d5a577841d11184d
luiz azevedo <lwnazevedo@gmail.com>
2022-03-18 16:03:58 -0300

    add new configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
   framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -28,13 +29,18 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
-      // postcss(),
+      postcss(),
+      babel({
+        exclude: "node_modules/**",
+        plugins: ["babel-plugin-styled-components"],
+      }),
     ],
-    external: ["react", "react-dom", "styled-components"],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: ["react", "react-dom", "styled-components", /\.(css|less|scss)$/],
+    globals: { "styled-components": "styled" },
   },
 ];
